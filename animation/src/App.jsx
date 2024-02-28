ccd4a7f341f97c0d3cd5373ab7723fd7fb68a0cd
antonnprince <antonprince95@gmail.com>
2024-02-28 21:21:01 +0530

    guitar playing animation

--- animation/src/App.jsx
+++ animation/src/App.jsx
@@ -4,7 +4,8 @@ import Experience from "./component/Experience"
 function App() {
   return (
     <div style={{height: '100vh'}}>
-      <Canvas>
+      <Canvas camera={{ position: [1, -3, 2.5], fov: 50 }} 
+      shadows>
         <Experience />
       </Canvas>
       </div>
--- animation/src/component/Experience.jsx
+++ animation/src/component/Experience.jsx
@@ -1,16 +1,30 @@
 import { OrbitControls } from "@react-three/drei";
-
+import Woman from "./Woman";
 
 const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-            <meshBasicMaterial />
-            <boxGeometry />
-        </mesh>
+      <ambientLight />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <group position={[0, -1, 0]}>
+        <Woman />
+      </group>
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
     </>
   );
 };
 
-export default Experience
+export default Experience;
